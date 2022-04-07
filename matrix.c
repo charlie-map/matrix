5957df4bcfe9cd5477a41e45d5c1d4e8b215ae42
charlie-map <charjhall777@gmail.com>
2022-04-06 20:57:42 -0400

    add-sub refactor and multiply

--- matrix.c
+++ matrix.c
@@ -3,103 +3,102 @@
 #include <stdarg.h>
 
 struct Matrix {
-	int *length, dimension_num;
-	void *matrix;
+	// length of rows and cols
+	int row, col;
+
+	float **matrix;
 };
 
-void set_sub_matrix(void *matrix, int *len, int curr_dim, int max_dim) {
-	if (curr_dim >= max_dim)
-		return;
+/*
+	builds and returns a simple matrix:
 
-	for (int setter = 0; setter < len[curr_dim - 1]; setter++) {
-		matrix[setter] = malloc(sizeof(void *) * len[curr_dim]);
-		set_sub_matrix(matrix[setter], len, curr_dim + 1, max_dim);
-	}
-}
+	row and col define the dimensions of the given matrix
+*/
+matrix_t *matrix_build(int row, int col) {
+	float **matrix = malloc(sizeof(float *) * row);
 
-matrix_t *matrix_build(int dimension_num, ...) {
-	va_list build_arg;
-	va_start(build_arg, dimension_num);
-
-	int *len = malloc(sizeof(int) * dimension_num);
-	for (int set_matrix = 0; set_matrix < dimension_num; set_matrix++)
-		len[set_matrix] = va_arg(build_arg, int);
-
-	void *matrix = malloc(sizeof(void *) * len[0]);
-	set_sub_matrix(matrix, len, 1, dimension_num);
-
-	matrix_t *m = malloc(sizeof(matrix_t));
-
-	m->dimension_num = dimension_num;
-	m->length = len;
-
-	m->matrix = matrix;
-
-	return m;
-}
-
-int matrix_update(matrix_t *m, ...) {
-	va_list update_arg;
-	va_start(update_arg, m);
-
-	void *matrix_mover = m->matrix;
-	int dimension_pos;
-	for (int dimension = 0; dimension < dimension_num; dimension++) {
-
-		dimension_pos = va_arg(update_arg, int);
-
-		matrix_mover = ((void **) matrix_mover)[dimension_pos];
+	for (int set_col = 0; set_col < row; set_col++) {
+		matrix[set_col] = malloc(sizeof(float) * col);
 	}
 
-	m->matrix_set(matrix_mover, va_arg(update_arg, void *));
+	matrix_t *r_m = malloc(sizeof(matrix_t));
 
-	return 0;
+	r_m->row = row;
+	r_m->col = col;
+
+	r_m->matrix = matrix;
+
+	return r_m;
 }
 
-int matrix_add_helper(void *r_m, void *m1, void *m2, int *len, int curr_dim, int max_dim) {
-	if (curr_dim >= max_dim)
+int check_dimensions_add_sub(matrix_t *m1, matrix_t *m2) {
+	// check dimensions
+	if (m1->row != m2->row)
 		return 0;
 
-	// level of computation
-	if (curr_dim == max_dim - 1) {
-		for (int add = 0; add < len[curr_dim]; add++) {
-			r_m[add] = m1[add] + m2[add];
+	if (m1->col != m2->col)
+		return 0;
+
+	return 1;
+}
+
+int matrix_add_sub_helper(matrix_t *r_m, matrix_t *m1, matrix_t *m2, int is_add) {
+	for (int add_row = 0; add_row < m1->row; add_row++) {
+
+		for (int add_col = 0; add_col < m1->col; add_col++) {
+			r_m->matrix[add_row][add_col] = is_add ? m1->matrix[add_row][add_col] + m1->matrix[add_row][add_col] :
+			m1->matrix[add_row][add_col] - m1->matrix[add_row][add_col];
 		}
-
-		return 0;
-	}
-
-	for (int sub_add = 0; sub_add < len[curr_dim] sub_add++) {
-		matrix_add_helper(r_m[sub_add], m1[sub_add], m2[sub_add], len, curr_dim + 1, max_dim);
 	}
 
 	return 0;
 }
 
 matrix_t *matrix_add(matrix_t *m1, matrix_t *m2) {
-	// check dimensions
-	if (m1->dimension_num != m2->dimension_num)
+	if (!check_dimensions_add_sub(m1, m2))
 		return NULL;
 
-	for (int check_dim_len = 0; check_dim_len < m1->dimension_num; check_dim_len++) {
-		if (m1->length[check_dim_len] != m2->length[check_dim_len])
-			return NULL;
-	}
+	matrix_t *r_m = matrix_build(m1->row, m1->col);
 
-	// build return matrix
-	matrix_t *r_m = matrix_build(m1->dimension_num, m1->length);
-
-	matrix_add_helper(r_m, m1, m2, m1->length, 0, m1->dimension_num);
+	matrix_add_sub_helper(r_m, m1, m2, 1);
 
 	return r_m;
 }
 
 matrix_t *matrix_subtract(matrix_t *m1, matrix_t *m2) {
-	
+	if (!check_dimensions_add_sub(m1, m2))
+		return NULL;
+
+	matrix_t *r_m = matrix_build(m1->row, m1->col);
+
+	matrix_add_sub_helper(r_m, m1, m2, 0);
+
+	return r_m;
+}
+
+int check_dimensions_multi(matrix_t *m1, matrix_t *m2) {
+	if (m1->col != m2->row)
+		return 0;
+
+	return 1;
 }
 
 matrix_t *matrix_multiply(matrix_t *m1, matrix_t *m2) {
+	if (!check_dimensions_multi(m1, m2))
+		return NULL;
 
+	matrix_t *r_m = matrix_build(m1->row, m2->col);
+	for (int calc_row = 0; calc_row < m1->row; calc_row++) {
+		for (int calc_col = 0; calc_col < m2->col; calc_col++) {
+
+			r_m->matrix[calc_row][calc_col] = 0;
+			for (int multi = 0; multi < m1->col; multi++) {
+				r_m->matrix[calc_row][calc_col] += m1->matrix[calc_row][multi] * m2->matrix[multi][calc_col];
+			}
+		}
+	}
+
+	return r_m;
 }
 
 matrix_t *matrix_divide(matrix_t *m1, matrix_t *m2) {
